de9f38a9b0eae85451adb6d91493694535e7e8c5
Marcus Willock <crazcalm@gmail.com>
2018-05-23 00:13:49 -0400

    Added a binarySearchTree

---
+++ data_structures/bst/bst.go
@@ -0,0 +1,185 @@
+package bst
+
+import (
+//"fmt"
+)
+
+//Element -- Element of a binary tree
+type Element struct {
+	Key                 uint32
+	Value               interface{}
+	Parent, Left, Right *Element
+}
+
+//BinarySearchTree -- BST
+type BinarySearchTree struct {
+	Root *Element
+}
+
+//NewTree -- returns a bst tree
+func NewTree() *BinarySearchTree {
+	return new(BinarySearchTree)
+}
+
+//RecursiveSearch --
+func RecursiveSearch(cur *Element, key uint32) *Element {
+	//Base Case: Did not find anything
+	if cur == nil {
+		return nil
+	}
+
+	//Base Case: Found it!
+	if cur.Key == key {
+		return cur
+	}
+
+	//Recursive cases
+	if key < cur.Key {
+		return RecursiveSearch(cur.Left, key)
+	}
+	return RecursiveSearch(cur.Right, key)
+}
+
+//Search -- Searches for element by key
+func (t *BinarySearchTree) Search(key uint32) *Element {
+	for cur := t.Root; cur != nil; {
+		if cur.Key == key {
+			return cur
+		} else if key < cur.Key {
+			cur = cur.Left
+		} else {
+			cur = cur.Right
+		}
+	}
+	return nil
+}
+
+//RecursiveInsert --
+func RecursiveInsert(cur, node *Element, tree *BinarySearchTree) *BinarySearchTree {
+	//Base Case: Root is nil. Need to Create a new tree
+	if cur == nil {
+		cur = node
+		return &BinarySearchTree{Root: &Element{Value: node.Value, Key: node.Key, Parent: nil, Left: nil, Right: nil}}
+	}
+
+	if cur.Left == nil && node.Key < cur.Key {
+		cur.Left = node
+		node.Parent = cur
+		return tree
+	}
+
+	if cur.Right == nil && node.Key >= cur.Key {
+		cur.Right = node
+		node.Parent = cur
+		return tree
+	}
+
+	//Recursive cases
+	if node.Key < cur.Key {
+		return RecursiveInsert(cur.Left, node, tree)
+	}
+	return RecursiveInsert(cur.Right, node, tree)
+
+}
+
+//Insert -- adds a new element to the tree
+func (t *BinarySearchTree) Insert(n *Element) *Element {
+	var target *Element
+
+	//Find suitable target node to unload this new node to
+	for cur := t.Root; cur != nil; {
+		target = cur
+		if n.Key < cur.Key {
+			cur = cur.Left
+		} else {
+			cur = cur.Right
+		}
+	}
+
+	n.Parent = target
+	if target == nil {
+		t.Root = n
+	} else if n.Key < target.Key {
+		target.Left = n
+	} else {
+		target.Right = n
+	}
+	return n
+}
+
+//Delete -- removes an item from the tree
+func (t *BinarySearchTree) Delete(key uint32) *Element {
+	deleteNonCompleteNode := func(node *Element) {
+		var reConnectedNode *Element
+		if node.Left == nil {
+			reConnectedNode = node.Right
+		} else {
+			reConnectedNode = node.Left
+		}
+
+		if reConnectedNode != nil {
+			reConnectedNode.Parent = node.Parent
+		}
+
+		if node.Parent == nil {
+			t.Root = reConnectedNode
+		} else if node.Parent.Right == node {
+			node.Parent.Right = reConnectedNode
+		} else {
+			node.Parent.Left = reConnectedNode
+		}
+		node = nil
+	}
+
+	node := t.Search(key)
+	if node == nil { //no Node to delete
+		return node
+	}
+
+	if node.Left == nil || node.Right == nil {
+		deleteNonCompleteNode(node)
+	} else {
+		successor := t.Successor(node, t.Root)
+		_key, _value := successor.Key, successor.Value
+		deleteNonCompleteNode(successor)
+		node.Key, node.Value = _key, _value
+	}
+	return node
+}
+
+//RecursiveMin --
+func RecursiveMin(cur *Element) *Element {
+	if cur == nil {
+		return nil
+	}
+
+	if cur.Left == nil {
+		return cur
+	}
+
+	return RecursiveMin(cur.Left)
+}
+
+//Min -- Returns the element with the smallest key
+func (t *BinarySearchTree) Min(cur *Element) *Element {
+	for cur.Left != nil {
+		cur = cur.Left
+	}
+	return cur
+}
+
+//Successor -- Used to help with deletion
+func (t *BinarySearchTree) Successor(n, root *Element) *Element {
+	if n == nil {
+		return nil
+	}
+	if n.Right != nil {
+		return t.Min(n.Right)
+	}
+	cur := n
+	for cur.Parent != nil && cur.Parent.Left != cur {
+		cur = cur.Parent
+	}
+	return cur.Parent
+
+}
---
+++ data_structures/bst/bst_test.go
@@ -0,0 +1,121 @@
+package bst
+
+import (
+	"testing"
+)
+
+func TestNewTree(t *testing.T) {
+	NewTree()
+}
+
+func TestRecursiveMin(t *testing.T) {
+	bst := NewTree()
+
+	data := []uint32{5, 6, 4, 7, 3, 8, 2, 10, 1}
+	var expected uint32 = 1
+
+	for _, v := range data {
+		element := &Element{Key: v, Value: v, Parent: nil, Right: nil, Left: nil}
+		bst.Insert(element)
+	}
+
+	result := RecursiveMin(bst.Root)
+	if result.Value.(uint32) != expected {
+		t.Errorf("Expected %d, but got %v", expected, result)
+	}
+
+}
+
+func TestMin(t *testing.T) {
+	bst := NewTree()
+
+	data := []uint32{5, 6, 4, 7, 3, 8, 2, 10, 1}
+	var expected uint32 = 1
+
+	for _, v := range data {
+		element := &Element{Key: v, Value: v, Parent: nil, Right: nil, Left: nil}
+		bst.Insert(element)
+	}
+
+	result := bst.Min(bst.Root)
+	if result.Value.(uint32) != expected {
+		t.Errorf("Expected %d, but got %v", expected, result)
+	}
+
+}
+
+func TestRecursiveInsert(t *testing.T) {
+	bst := NewTree()
+
+	data := []uint32{3, 5, 4, 2, 1}
+	for _, v := range data {
+		element := &Element{Key: v, Value: v, Parent: nil, Right: nil, Left: nil}
+		bst = RecursiveInsert(bst.Root, element, bst)
+	}
+
+	for _, v := range data {
+		result := RecursiveSearch(bst.Root, v)
+
+		if v != result.Value.(uint32) {
+			t.Errorf("Expected node to have Value = %d, but got %v", v, result)
+		}
+	}
+}
+
+func TestInsert(t *testing.T) {
+	bst := NewTree()
+
+	data := []uint32{1, 2, 3, 4, 5}
+	for _, v := range data {
+		element := &Element{Key: v, Value: v, Parent: nil, Right: nil, Left: nil}
+		bst.Insert(element)
+	}
+
+	for _, v := range data {
+		result := bst.Search(v)
+
+		if v != result.Value.(uint32) {
+			t.Errorf("Expected node to have Value = %d, but got %v", v, result)
+		}
+	}
+}
+
+func TestRecursiveSearch(t *testing.T) {
+	bst := NewTree()
+
+	data := []uint32{1, 2, 3, 4, 5}
+	for _, v := range data {
+		element := &Element{Key: v, Value: v, Parent: nil, Right: nil, Left: nil}
+		bst.Insert(element)
+	}
+
+	for _, v := range data {
+		result := RecursiveSearch(bst.Root, v)
+
+		if v != result.Value.(uint32) {
+			t.Errorf("Expected node to have Value = %d, but got %v", v, result)
+		}
+	}
+}
+
+func TestDelete(t *testing.T) {
+	bst := NewTree()
+
+	data := []uint32{1, 2, 3, 4, 5}
+	for _, v := range data {
+		element := &Element{Key: v, Value: v, Parent: nil, Right: nil, Left: nil}
+		bst.Insert(element)
+	}
+
+	for _, v := range data {
+		result := bst.Delete(v)
+		if result == nil {
+			t.Errorf("Failed to delete element with key %d", v)
+		}
+	}
+
+	//Tree should be empty
+	if bst.Root != nil {
+		t.Errorf("Expected the tree to be empty, but it was not: %v", bst)
+	}
+}
